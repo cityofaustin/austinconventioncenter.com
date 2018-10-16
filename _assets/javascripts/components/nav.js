0e65bd73e235becb72661672e00449f6de2000b8
Matt Langan <mattlangan@users.noreply.github.com>
2018-10-16 10:38:29 -0500

    add Close button to subnav megamenus

--- _assets/javascripts/components/nav.js
+++ _assets/javascripts/components/nav.js
@@ -9,3 +9,11 @@ function navInit (delegate) {
 }
 
 module.exports.init = navInit;
+
+$(document).ready(function() {
+  $('.acc-nav-close-button').click(function() {
+    event.preventDefault();
+    $(this).parent('div').attr('aria-hidden', 'true');
+    $(this).parent('div').prev('button').attr('aria-expanded', 'false');
+  });
+});
