70aa5685acd4ab57c9d25fc16c88b4b402efdddf
clubajax <anm9tr@gmail.com>
2019-01-31 16:22:55 -0600

    Final navigation styling

--- _assets/javascripts/components/nav.js
+++ _assets/javascripts/components/nav.js
@@ -1,19 +1,20 @@
 function toggleNav (event) {
   document.querySelector(".acc-nav-button").classList.toggle("is-active");
-  document.querySelector(".acc-nav").classList.toggle("is-visible");
+  document.querySelector(".acc-nav").classList.toggle("is-visible"); // main menu
   event.preventDefault();
 }
 
 function navInit (delegate) {
-  delegate.on("click", ".acc-nav-button", toggleNav);
+  delegate.on("click", ".acc-nav-button", toggleNav); // hamburger
 }
 
-module.exports.init = navInit;
-
-$(document).ready(function() {
-  $('.acc-nav-close-button').click(function() {
+$(document).ready(function () {
+  $('.acc-nav-close-button').click(function () {
+    console.log('CLICK');
     event.preventDefault();
-    $(this).parent('div').attr('aria-hidden', 'true');
-    $(this).parent('div').prev('button').attr('aria-expanded', 'false');
+    $(this).closest('.acc-nav-section').attr('aria-hidden', 'true');
+    $(this).closest('.acc-nav-section').prev('button').attr('aria-expanded', 'false');
   });
 });
+
+module.exports.init = navInit;
