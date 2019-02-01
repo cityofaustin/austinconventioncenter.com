148f0acbab6334c039ef02cd3a6d6af1146a6dd3
clubajax <anm9tr@gmail.com>
2019-02-01 11:49:29 -0600

    remove/internalize vendor/usdws; style mobile menu

--- _assets/javascripts/components/nav.js
+++ _assets/javascripts/components/nav.js
@@ -1,16 +1,15 @@
 function toggleNav (event) {
-  document.querySelector(".acc-nav-button").classList.toggle("is-active");
+  document.querySelector("#menu-button").classList.toggle("is-active");
   document.querySelector(".acc-nav").classList.toggle("is-visible"); // main menu
   event.preventDefault();
 }
 
 function navInit (delegate) {
-  delegate.on("click", ".acc-nav-button", toggleNav); // hamburger
+  delegate.on("click", "#menu-button", toggleNav); // hamburger
 }
 
 $(document).ready(function () {
   $('.acc-nav-close-button').click(function () {
-    console.log('CLICK');
     event.preventDefault();
     $(this).closest('.acc-nav-section').attr('aria-hidden', 'true');
     $(this).closest('.acc-nav-section').prev('button').attr('aria-expanded', 'false');
