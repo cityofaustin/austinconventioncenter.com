3915ed926851a1378a21a7dd3a727af96135f1ca
Matt Langan <mattlangan@users.noreply.github.com>
2020-03-17 12:24:16 -0500

    Add support for Press Release photos

--- _plugins/generators/contentful.rb
+++ _plugins/generators/contentful.rb
@@ -36,6 +36,7 @@ module Jekyll
           attributes["date"] = attributes["date"].utc # Undo implicit time zone conversion
           page = generate_page(site, press_releases_section, attributes)
           page.data["breadcrumbs"][-1]["title"] = attributes["date"].strftime("%B %-d, %Y")
+          page.data["photo_url"] = attributes["picture"]["url"] if attributes["picture"].present?
         end
       end
     end
