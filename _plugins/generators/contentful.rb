7516666652530317a0f8fc3c7736eec79d18edc6
Matt Langan <mattlangan@users.noreply.github.com>
2020-05-05 07:09:38 -0500

    Introduce "Newsroom" nav construct
    A Newsroom navigation item has been added to provide greater flexibility around corporate communications/messaging. The initial release includes Press Releases (formerly Newsroom, formerly Press Room, formerly Press Releases) and a new "In The News" section, for 3rd-party articles/media written _about_ ACCD instead of _by_ ACCD.
    
    Future Newsroom pages will include Expansion Plans and likely some contact/Meet The Team stuff.
    
    This required additions and edits to the Contentful Content Models.

--- _plugins/generators/contentful.rb
+++ _plugins/generators/contentful.rb
@@ -31,13 +31,36 @@ module Jekyll
         generate_page(site, section, attributes)
       end
 
-      if press_releases_section = site.collections["newsroom"]
+      if press_releases_section = site.collections["press-releases"]
         data.fetch("pressRelease", []).each do |attributes|
+#           puts "doing press release item fetch"
           attributes["date"] = attributes["date"].utc # Undo implicit time zone conversion
           page = generate_page(site, press_releases_section, attributes)
           page.data["breadcrumbs"][-1]["title"] = attributes["date"].strftime("%B %-d, %Y")
           page.data["photo_url"] = attributes["picture"]["url"] if attributes["picture"].present?
           page.data["preview_text"] = attributes["previewText"] if attributes["previewText"].present?
+        end
+      end
+
+#       site.collections.each do |c|
+#         puts "Collection:"
+#         puts c
+#         puts "^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^"
+#       end
+      #puts "hello between"
+      #puts "#{site.collections}"
+      #puts "done bwteen"
+
+      if in_the_news_section = site.collections["in-the-news"]
+        data.fetch("inTheNews", []).each do |attributes|
+          puts "in the news item. #{attributes}"
+          puts "* * * * * *"
+          attributes["date"] = attributes["date"].utc # Undo implicit time zone conversion
+          page = generate_page(site, in_the_news_section, attributes)
+          page.data["breadcrumbs"][-1]["title"] = attributes["date"].strftime("%B %-d, %Y")
+          page.data["photo_url"] = attributes["photo"]["url"] if attributes["photo"].present?
+          page.data["preview_text"] = attributes["previewText"] if attributes["previewText"].present?
+          page.data["publication_url"] = attributes["publicationUrl"]
         end
       end
     end
