163198786b885260a5e84d61fab2300549a70193
ACCDHelpdesk <accd.helpdesk@austintexas.gov>
2025-05-07 13:42:36 -0500

    Update import.rb
    Hardcoded the Azure URL for our event calendar data. Bypassing the enviroment variable [CALENDAR_DATA_SOURCE]

--- lib/calendar/import.rb
+++ lib/calendar/import.rb
@@ -8,7 +8,7 @@ module Calendar
 
     class << self
       def import(config)
-        results = HTTParty.get(ENV["CALENDAR_DATA_SOURCE"])
+        results = HTTParty.get('https://accdcalendar.blob.core.windows.net/webevents/calendarevents.json?sp=r&st=2025-04-25T13:03:04Z&se=2029-04-25T21:03:04Z&spr=https&sv=2024-11-04&sr=b&sig=Tag0DwvhSueuk1rzdJsYjLYyUcA1aC4CzP9OEU6ncP8%3D')
         path = File.join(config["source"], config["data_dir"], "socrata", "events", "events.yaml")
 
         FileUtils.mkdir_p(File.dirname(path))
